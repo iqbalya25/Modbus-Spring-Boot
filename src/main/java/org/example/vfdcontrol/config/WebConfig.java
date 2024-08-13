f303fc758fc2a19fc49f4e44af6f3120b5b8e515
iqbalya25 <iqbalya25@gmail.com>
2024-08-13 17:18:33 +0700

    change cors

--- src/main/java/org/example/vfdcontrol/config/WebConfig.java
+++ src/main/java/org/example/vfdcontrol/config/WebConfig.java
@@ -7,7 +7,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("plccontrolmodbusiot-git-plcmodbus-iqbalya25s-projects.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
